fe3c13a0943dd34be5cdf017e2917105dd925ac0
Farahinha00 <151730453+Farahinha00@users.noreply.github.com>
2024-01-14 18:54:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import tensorflow as tf
+import joblib
+import requests
+from script_inf import make_inference  # Assure-toi que script_inf.py est dans le même dossier
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Charger le modèle et le tokenizer
+best_model = tf.keras.models.load_model('https://ocp7.blob.core.windows.net/common/mon_best_model.h5')
+tokenizer = joblib.load('https://ocp7.blob.core.windows.net/common/tokenizer.pkl')
+
+@app.get("/predict/{raw_data}")
+async def predict(raw_data: str):
+    # Effectuer l'inférence
+    prediction = make_inference(raw_data)
+
+    # Retourner la prédiction
+    return {"Prédiction": prediction}
