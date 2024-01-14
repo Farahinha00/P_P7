9d01a21deb34ec4a82d1d89984e3a74020fa2d13
Farahinha00 <151730453+Farahinha00@users.noreply.github.com>
2024-01-14 16:25:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-uvicorn.run("app:app", host="0.0.0.0", port=8080)
