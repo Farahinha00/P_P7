1abc7a04b2534d60fa4c9c0f4ebbc31cb37fb7ec
Farahinha00 <151730453+Farahinha00@users.noreply.github.com>
2024-01-19 11:38:15 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,45 @@
 from fastapi import FastAPI
 import tensorflow as tf
 import joblib
-import requests
-from script_inf import make_inference  # Assure-toi que script_inf.py est dans le même dossier
+from azureml.core.model import Model
+from azureml.core import Workspace
+from script_inf import make_inference  # Assurez-vous que script_inf.py est dans le même dossier
 
 app = FastAPI()
 
-# Charger le modèle et le tokenizer
-best_model = tf.keras.models.load_model('https://ocp7.blob.core.windows.net/common/mon_best_model.h5')
-tokenizer = joblib.load('https://ocp7.blob.core.windows.net/common/tokenizer.pkl')
+# Déclaration des variables globales
+model = None
+tokenise = None
+ws = Workspace.from_config() 
+def init():
+    global model
+    global tokenise
+
+    # Obtenez le chemin des modèles stockés dans Azure ML
+    best_model_path = Model.get_model_path(model_name='best_model', version=1, _workspace=ws)
+    tokenizer_path = Model.get_model_path(model_name='tokenizer', version=1, _workspace=ws)
+
+    try:
+        # Chargez le tokenizer
+        tokenise = joblib.load(tokenizer_path)
+    except Exception as e:
+        print(f"Erreur lors du chargement du tokenizer : {e}")
+        
+    try:
+        # Chargez le modèle Keras
+        model = tf.keras.models.load_model(best_model_path)
+    except Exception as e:
+        print(f"Erreur lors du chargement du modèle Keras : {e}")
+
+# Événement de démarrage pour exécuter la fonction init
+@app.on_event("startup")
+async def startup_event():
+    init()
 
 @app.get("/predict/{raw_data}")
 async def predict(raw_data: str):
     # Effectuer l'inférence
-    prediction = make_inference(raw_data)
+    prediction = make_inference(raw_data,tokenise,model)
 
     # Retourner la prédiction
     return {"Prédiction": prediction}
--- script_inf.py
+++ script_inf.py
@@ -6,7 +6,10 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential, Model
 from keras.layers import Dense, Embedding, LSTM, Dropout, Flatten, SimpleRNN
 import joblib
-
+import lime
+from lime import lime_text
+from sklearn.pipeline import make_pipeline
+from lime.lime_text import LimeTextExplainer
 
 def tokenize_data(data,tokenizer,maxlen=None):
     X = tokenizer.texts_to_sequences(data)
@@ -15,20 +18,17 @@ def tokenize_data(data,tokenizer,maxlen=None):
 def model_predict(texts):
     # Assure-toi que 'texts' est prétraité de la même manière que les données d'entraînement
     processed_texts = tokenize_data(texts,tokenizer,maxlen=50)
-    return best_model.predict(processed_texts)
+    return model.predict(processed_texts)
 
-def make_inference(input_data):
+def make_inference(input_data,tokenizer,model):
     # Prédiction à l'aide du modèle
     processed_data = tokenize_data([input_data],tokenizer,maxlen=50)
-    prediction = best_model.predict(processed_data)
-
+    prediction = model.predict(processed_data)
+    prediction_value = float(prediction[0][0]) 
     # Affichage des résultats de la prédiction
-    print("Prédiction brute du modèle : ", prediction)
+    print("Prédiction brute du modèle : ", prediction_value)
+
     
-    explainer = LimeTextExplainer(class_names=['bad', 'good'])
-    exp = explainer.explain_instance(input_data, model_predict, num_features=10)
-    exp.show_in_notebook(text=True)
-    
-    return prediction, exp
+    return prediction_value
 
 
