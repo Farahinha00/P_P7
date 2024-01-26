9641d87280bee53eeb397c47cdfaba5cc15cb6f9
Farahinha00 <151730453+Farahinha00@users.noreply.github.com>
2024-01-26 13:16:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,38 +8,38 @@ from script_inf import make_inference  # Assurez-vous que script_inf.py est dans
 app = FastAPI()
 
 # Déclaration des variables globales
-model = None
-tokenise = None
-ws = Workspace.from_config() 
-def init():
-    global model
-    global tokenise
+# model = None
+# tokenise = None
+# ws = Workspace.from_config() 
+# def init():
+#     global model
+#     global tokenise
 
     # Obtenez le chemin des modèles stockés dans Azure ML
-    best_model_path = Model.get_model_path(model_name='best_model', version=1, _workspace=ws)
-    tokenizer_path = Model.get_model_path(model_name='tokenizer', version=1, _workspace=ws)
+#    best_model_path = Model.get_model_path(model_name='best_model', version=1, _workspace=ws)
+#     tokenizer_path = Model.get_model_path(model_name='tokenizer', version=1, _workspace=ws)
 
-    try:
+#     try:
         # Chargez le tokenizer
-        tokenise = joblib.load(tokenizer_path)
-    except Exception as e:
-        print(f"Erreur lors du chargement du tokenizer : {e}")
+ #        tokenise = joblib.load(tokenizer_path)
+#     except Exception as e:
+#         print(f"Erreur lors du chargement du tokenizer : {e}")
         
-    try:
+ #    try:
         # Chargez le modèle Keras
-        model = tf.keras.models.load_model(best_model_path)
-    except Exception as e:
-        print(f"Erreur lors du chargement du modèle Keras : {e}")
+  #       model = tf.keras.models.load_model(best_model_path)
+ #    except Exception as e:
+  #       print(f"Erreur lors du chargement du modèle Keras : {e}")
 
 # Événement de démarrage pour exécuter la fonction init
-@app.on_event("startup")
-async def startup_event():
-    init()
+# @app.on_event("startup")
+# async def startup_event():
+#     init()
 
-@app.post("/predict/{raw_data}")
-async def predict(raw_data: str):
+# @app.post("/predict/{raw_data}")
+# async def predict(raw_data: str):
     # Effectuer l'inférence
-    prediction = make_inference(raw_data,tokenise,model)
+ #    prediction = make_inference(raw_data,tokenise,model)
 
     # Retourner la prédiction
-    return {"Prédiction"}
+  #   return {"Prédiction"}
