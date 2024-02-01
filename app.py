cb089c42b4bb7715f42b866f37f91701ae9ccece
Farahinha00 <151730453+Farahinha00@users.noreply.github.com>
2024-02-01 10:12:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ from script_inf import make_inference  # Assurez-vous que script_inf.py est dans
 app = FastAPI()
 
 # Déclaration des variables globales
-model = None
-tokenise = None
-ws = Workspace.from_config()
+#model = None
+#tokenise = None
+#ws = Workspace.from_config()
 
-def init():
-    global model
-    global tokenise
+#def init():
+#    global model
+#    global tokenise
 
     # Obtenez le chemin des modèles stockés dans Azure ML
-    best_model_path = Model.get_model_path(model_name='best_model', version=1, _workspace=ws)
-    tokenizer_path = Model.get_model_path(model_name='tokenizer', version=1, _workspace=ws)
+#    best_model_path = Model.get_model_path(model_name='best_model', version=1, _workspace=ws)
+#    tokenizer_path = Model.get_model_path(model_name='tokenizer', version=1, _workspace=ws)
 
-    try:
-        # Chargez le tokenizer
-        tokenise = joblib.load(tokenizer_path)
-    except Exception as e:
-        print(f"Erreur lors du chargement du tokenizer : {e}")
-        
-    try:
-        # Chargez le modèle Keras
-        model = tf.keras.models.load_model(best_model_path)
-    except Exception as e:
-        print(f"Erreur lors du chargement du modèle Keras : {e}")
+#    try:
+#        # Chargez le tokenizer
+#        tokenise = joblib.load(tokenizer_path)
+#    except Exception as e:
+#        print(f"Erreur lors du chargement du tokenizer : {e}")
+#        
+#    try:
+#        # Chargez le modèle Keras
+#        model = tf.keras.models.load_model(best_model_path)
+#    except Exception as e:
+#        print(f"Erreur lors du chargement du modèle Keras : {e}")
 
 
 # Événement de démarrage pour exécuter la fonction init
