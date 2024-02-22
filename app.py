4c0e21d820147531374cce3cde5d591a9fd0828b
Farahinha00 <151730453+Farahinha00@users.noreply.github.com>
2024-02-22 12:41:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ async def startup_event():
 @app.get("/", response_class=HTMLResponse)
 async def get_form():
     with tracer.start_as_current_span("get_form")
+    logging.info("Displaying form")
     return """
     <html>
         <body>
@@ -84,7 +85,7 @@ async def predict(raw_data: str = Form(...)):
         sentiment = "good"
     else:
         sentiment = "bad"
-
+    logging.info(f"Received '{raw_data}' and predicted as '{sentiment}'")
     response = f'The tweet "{raw_data}" is {sentiment}.'
 
     return response
